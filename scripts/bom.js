a6bf9e0c49d715f0114b3e8fe72b118c477463b0
Rexcreamer <byuajw@gmail.com>
2022-11-29 15:54:13 -0700

    working on bom

---
+++ scripts/bom.js
@@ -0,0 +1,11 @@
+const inp = document.getElementById('favchap');
+const but = document.getElementById('button');
+const lis = document.getElementById('listcontainer');
+const lis2 = document.getElementById('list');
+
+button.addEventListener('click', function() {
+   const li = document.createElement('li');
+   li.innerHTML = inp;
+   lis2.appendChild(li);
+}
+);
