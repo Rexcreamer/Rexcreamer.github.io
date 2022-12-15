5836b1dbbeba214995c7bf31b453d821609f1c3f
Rexcreamer <byuajw@gmail.com>
2022-12-14 17:28:06 -0700

    ....

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,12 @@
+// Function to get temperature and windspeed, and calculate windchill
+function windChill(windSpeed, currentTemp) {
+    let windChill;
+    
+    if (windSpeed > 3 && currentTemp <= 50) {
+        windChill = 35.74 + 0.6215 * currentTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemp * windSpeed ** 0.16;
+        windChill = windChill.toFixed(0)
+    } else {
+        windChill = "NA";
+    }
+    document.querySelector("#wind-chill").innerHTML = windChill;
+}
