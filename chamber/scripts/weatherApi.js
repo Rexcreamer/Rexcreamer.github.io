925f2f9cc8bb290b1881b11cb818d1326a11036c
Rexcreamer <byuajw@gmail.com>
2022-12-14 17:12:19 -0700

    ...

--- chamber/scripts/weatherApi.js
+++ chamber/scripts/weatherApi.js
@@ -1,60 +1,37 @@
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?Tucon,AZ,US&appid=7085b59d4a786d720061217efd0eaa85"&units=imperial`;
-function showWeather(obj){
-    let currenttemp = document.querySelector('#current-temp');
-    let figurecaption = document.querySelector('#figcaption');
-    const iconURL = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
-    currenttemp.textContent = Math.round(obj.main.temp);
-    iconpath.textContent = iconURL;
-    weathericon.setAttribute("src", iconURL);
-    weathericon.setAttribute("alt", obj.weather[0].description);
-    figurecaption.textContent = obj.weather[0].main;
-}
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    showWeather(jsObject);
-  });
-
-  // Populate the DOM stuff
-  tempobj.textContent = temp;
-  windspeedobj.textContent = windspeed;
-  windchillobj.innerHTML = windchillmsg;
-
-  function setwindchill(temp, windspeed){
-    // References to DOM elements
-    let tempobj = document.querySelector("#current-temp");
-    let windspeedobj = document.querySelector("#windspeed");
-    let windchillobj = document.querySelector("#windchill");
-    
-    // Calculate windspeed if necessary
-    let windchillmsg = "N/A";
-    
-    if (temp <= 50 && windspeed > 3){
-        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-        windchillmsg = `${chill}&deg;`;
-    }
-    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-
-    let windSpeed = Math.round(result.wind.speed /.44704);
-
-    windchillobj.innerHTML = wind;
-    tempobj.innerHTML = fahrenheit;
-    windspeedobj.text(windSpeed);
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
   
-    setwindchill(39, 10);
-        if (weatherId >= 200 && weatherId < 531){
-            document.querySelector('#weathericon').setAttribute('src', 'imgs/rainy.png');
-        }
-        else if (weatherId >= 600 && weatherId < 622){
-            document.querySelector('#weathericon').setAttribute('src', 'imgs/snowy.png');
-        }
-        else if (weatherId >= 801 && weatherId < 805){
-            document.querySelector('#weathericon').setAttribute('src', 'imgs/cloudy.png');
-        }
-        else if (weatherId == 800){
-            document.querySelector('#weathericon').setAttribute('src', 'imgs/sunny.png');
-        }
-        else{
-            document.querySelector('#weathericon').setAttribute('src', 'imgs/sunny.png');
-        }};
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      lon = position.coords.longitude;
+      lat = position.coords.latitude;
+  
+      // API ID
+      const api = "6d055e39ee237af35ca066f35474e9df";
+  
+      // API URL
+      const base =`https://api.openweathermap.org/data/2.5/weather?Tucon,AZ,US&appid=7085b59d4a786d720061217efd0eaa85"&units=imperial`;
+  
+      // Calling the API
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          temperature.textContent = 
+              Math.floor(data.main.temp - kelvin) + "°C";
+          summary.textContent = data.weather[0].description;
+          loc.textContent = data.name + "," + data.sys.country;
+          let icon1 = data.weather[0].icon;
+          icon.innerHTML = 
+              `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+        });
+    });
+  }
+});
